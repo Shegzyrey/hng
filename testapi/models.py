7f45c25580cc7c89c16c72890886f3ab7098f283
shegzyrey <oluwasegun782@gmail.com>
2022-10-29 14:26:49 -0400

    changed page

--- testapi/models.py
+++ testapi/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class StudentData(models.Model):
-	slackusername = models.CharField(max_length=15)
+	slackUsername = models.CharField(max_length=15)
 	backend = models.BooleanField()
 	age = models.IntegerField()
 	bio = models.TextField(blank=True)
