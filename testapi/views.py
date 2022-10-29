66c32368cfa4214c5ab4b293f449248501feae4b
shegzyrey <oluwasegun782@gmail.com>
2022-10-29 14:11:54 -0400

    changed page

--- testapi/views.py
+++ testapi/views.py
@@ -5,10 +5,8 @@ from .models import StudentData
 from .serializers import StudentDataSerializer
 # Create your views here.
 def index(request):
-	return HttpResponse("<h1>The Meandco Homepage</h1>")
-
-def studentdata_list(request):
 	studentdata = StudentData.objects.all()
 	serializer = StudentDataSerializer(studentdata, many=True)
 
 	return JsonResponse(serializer.data, safe=False)
+
